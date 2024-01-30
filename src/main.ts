9602eab33fdafa086019ba55efac090b76495ed9
alex-kostenko <0984181760ss@gmail.com>
2024-01-30 15:31:54 +0200

    lint test

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  console.log(1);
   await app.listen(3000);
 }
 bootstrap();
