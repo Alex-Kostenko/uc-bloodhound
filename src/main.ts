197ebf376ee0c67a65950385291f93e30db7bc93
alex-kostenko <0984181760ss@gmail.com>
2024-02-12 16:46:49 +0200

    Add logs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,9 +17,28 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  const swaggerPath = 'api';
 
-  await app.listen(3000);
+  SwaggerModule.setup(swaggerPath, app, document);
+
+  const port = 3000;
+
+  await app.listen(port);
+
+  Logger.debug(
+    '==========================================================================',
+  );
+  Logger.warn(
+    `⚓️ SWAGGER HAS BEEN STARTED: http://localhost:${port}/${swaggerPath} ⚓️`,
+  );
+  Logger.debug(
+    '==========================================================================',
+  );
+  Logger.warn(`⚡ PROJECT HAS BEEN STARTED: http://localhost:${port} ⚡`);
+  Logger.debug(
+    '==========================================================================',
+  );
+  Logger.debug('HAPPY HACKING 👾');
 }
 
 bootstrap();
